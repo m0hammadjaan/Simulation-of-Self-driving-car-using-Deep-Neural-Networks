0479cd42fa179b9fcc0644cd5d31b7722c86a00b
m0hammadjaan <janmuhammad2050@gmail.com>
2023-02-16 01:18:40 +0500

    Collision

--- interface.js
+++ interface.js
@@ -3,12 +3,22 @@ canvas.width=470;
 
 const ctx = canvas.getContext("2d");
 const road=new Road(canvas.width/2,canvas.width*0.9, 7);
-const car=new Car(road.getLaneCenter(3),100,30,50);
+const car=new Car(road.getLaneCenter(3),100,30,50, "Player");
+const traffic = 
+[
+    new Car(road.getLaneCenter(random()),-100,30,50, "DUMMY", 2, "red")
+];
+
 
 animate();
 
 function animate(){
-    car.update(road.borders);
+for(let i = 0; i < traffic.length; i++)
+{
+    traffic[i].update(road.borders, []);
+}
+
+    car.update(road.borders, traffic);
 
     canvas.height=window.innerHeight;
 
@@ -16,6 +26,10 @@ function animate(){
     ctx.translate(0,-car.y+canvas.height*0.7);
 
     road.draw(ctx);
+    for(let i = 0; i < traffic.length; i++)
+    {
+        traffic[i].draw(ctx);
+    }
     car.draw(ctx);
 
     ctx.restore();
