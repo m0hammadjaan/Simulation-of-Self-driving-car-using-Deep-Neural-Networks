4f926e95005731b86184d8d75407512ff3f38651
m0hammadjaan <janmuhammad2050@gmail.com>
2023-03-03 22:46:22 +0500

    SpeedoMeter removed for the time being

--- speedoMeter.js
+++ speedoMeter.js
@@ -3,12 +3,5 @@ class SpeedoMeter
     constructor(car)
     {
         this.car = car;
-        this.spread = Math.PI*2
-    }
-    draw(ctx)
-    {
-        ctx.beginPath();
-        ctx.arc(100, 75, 50, 0, 2 * Math.PI);
-        ctx.stroke();
     }
 }
