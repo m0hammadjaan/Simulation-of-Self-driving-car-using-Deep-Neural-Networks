27381be7ae26d5e7938e9e4f9a09295479f85f1e
Muhammad Jan <jan4205668@cloud.neduet.edu.pk>
2023-04-26 18:21:09 +0500

    added utils.py

---
+++ utils.py
@@ -0,0 +1,11 @@
+learningRate = 0.0001
+gamma = 0.95
+epsilon = 1.0
+memSize = 20
+epsilonMinimum = 0.1
+batchSize = 32
+replace = 1000
+epsilonDecrease = 1e-5
+checkpointDirectory = 'models/'
+algorithm = 'DQN'
+envName = 'highway-v0'
