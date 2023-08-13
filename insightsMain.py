56167287d1aff6947db9323bb1dc20d4fc317551
m0hammadjaan <janmuhammad2050@mail.com>
2023-08-13 15:14:48 -0700

    insights interface implemented

--- insightsMain.py
+++ insightsMain.py
@@ -1,2 +1,9 @@
+from Insights import Insights
 
-
+insights = Insights('RSAgentPerformance.csv')
+insights.plotEpisodeScore()
+insights.plotAverageScore()
+insights.plotBestScore()
+insights.plotEpsilon()
+insights.plotMultipleFeatures('Score', 'AverageScore', 'BestScore')
+insights.plotSteps()
